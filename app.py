e5cade0878767f05d40333b6c731f27e533598f3
Ubuntu <ubuntu@ip-172-31-2-159.ap-south-1.compute.internal>
2026-09-06 10:49:09 +0000

    Update application and Docker configuration

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def predict():
     return jsonify({"stress_level": int(prediction[0])})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=False)
